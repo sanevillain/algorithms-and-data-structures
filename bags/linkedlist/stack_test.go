0a7bf185e65037bf37ccc3971c7ae7b3b11808e0
sanevillain <denis.ismail@code.berlin>
2023-02-12 12:47:30 +0100

    clean up stack test cases a bit

--- bags/linkedlist/stack_test.go
+++ bags/linkedlist/stack_test.go
@@ -18,12 +18,8 @@ func TestStack_New(t *testing.T) {
 
 func TestStack_Push(t *testing.T) {
 	t.Run("it should push items to the stack", func(t *testing.T) {
-		s := NewStack[int]()
-
 		size := 10
-		for i := 0; i < size; i++ {
-			s.Push(i)
-		}
+		s, _ := newIntStack(size)
 
 		if s.Size() != size {
 			t.Errorf("s.Size() = %v, want %v", s.Size(), size)
@@ -33,12 +29,7 @@ func TestStack_Push(t *testing.T) {
 
 func TestStack_Pop(t *testing.T) {
 	t.Run("it should pop items from the stack in the reverse order of insertion", func(t *testing.T) {
-		s := NewStack[int]()
-
-		vals := []int{1, 2, 3, 4, 5}
-		for _, v := range vals {
-			s.Push(v)
-		}
+		s, vals := newIntStack(5)
 
 		for i := len(vals) - 1; i >= 0; i-- {
 			if pop := s.Pop(); pop != vals[i] {
@@ -82,14 +73,8 @@ func TestStack_Peek(t *testing.T) {
 }
 
 func TestStack_Copy(t *testing.T) {
-	t.Run("it should copy the stack and its items in the correct order", func(t *testing.T) {
-		s := NewStack[int]()
-
-		vals := []int{1, 2, 3, 4, 5}
-		for _, v := range vals {
-			s.Push(v)
-		}
-
+	t.Run("it should copy the stack and its items in the correct order without modifying the underlying stack", func(t *testing.T) {
+		s, vals := newIntStack(5)
 		s2 := s.Copy()
 
 		for i := len(vals) - 1; i >= 0; i-- {
@@ -97,17 +82,18 @@ func TestStack_Copy(t *testing.T) {
 				t.Errorf("s2.Pop() = %v, want %v", pop, vals[i])
 			}
 		}
+
+		for i := len(vals) - 1; i >= 0; i-- {
+			if pop := s.Pop(); pop != vals[i] {
+				t.Errorf("s.Pop() = %v, want %v", pop, vals[i])
+			}
+		}
 	})
 }
 
 func TestStack_Delete(t *testing.T) {
 	t.Run("it shouldnt impact stack if called with an index beyond the stack items", func(t *testing.T) {
-		s := NewStack[int]()
-
-		vals := []int{1, 2, 3, 4, 5}
-		for _, v := range vals {
-			s.Push(v)
-		}
+		s, vals := newIntStack(5)
 
 		s.Delete(-1)
 		s.Delete(10)
@@ -118,80 +104,53 @@ func TestStack_Delete(t *testing.T) {
 	})
 
 	t.Run("it should delete the first item and set the correct head", func(t *testing.T) {
-		s := NewStack[int]()
-
-		item1 := 1
-		item2 := 2
-		item3 := 3
-
-		s.Push(item1)
-		s.Push(item2)
-		s.Push(item3)
-
+		s, _ := newIntStack(3)
 		s.Delete(0)
 
-		if s.Size() != 2 {
-			t.Errorf("s.Size() = %v, want %v", s.Size(), 2)
+		if size := s.Size(); size != 2 {
+			t.Errorf("s.Size() = %v, want %v", size, 2)
 		}
 
-		if s.Pop() != item2 {
-			t.Errorf("s.Peek() = %v, want %v", s.Peek(), item2)
+		if pop := s.Pop(); pop != 2 {
+			t.Errorf("s.Peek() = %v, want %v", pop, 2)
 		}
 
-		if s.Pop() != item1 {
-			t.Errorf("s.Peek() = %v, want %v", s.Peek(), item1)
+		if pop := s.Pop(); pop != 1 {
+			t.Errorf("s.Peek() = %v, want %v", pop, 1)
 		}
 	})
 
 	t.Run("it should delete the second item and set the correct head", func(t *testing.T) {
-		s := NewStack[int]()
-
-		item1 := 1
-		item2 := 2
-		item3 := 3
-
-		s.Push(item1)
-		s.Push(item2)
-		s.Push(item3)
-
+		s, _ := newIntStack(3)
 		s.Delete(1)
 
-		if s.Size() != 2 {
-			t.Errorf("s.Size() = %v, want %v", s.Size(), 2)
+		if size := s.Size(); size != 2 {
+			t.Errorf("s.Size() = %v, want %v", size, 2)
 		}
 
-		if s.Pop() != item3 {
-			t.Errorf("s.Peek() = %v, want %v", s.Peek(), item3)
+		if pop := s.Pop(); pop != 3 {
+			t.Errorf("s.Peek() = %v, want %v", pop, 3)
 		}
 
-		if s.Pop() != item1 {
-			t.Errorf("s.Peek() = %v, want %v", s.Peek(), item1)
+		if pop := s.Pop(); pop != 1 {
+			t.Errorf("s.Peek() = %v, want %v", pop, 1)
 		}
 	})
 
 	t.Run("it should delete the third item and set the correct head", func(t *testing.T) {
-		s := NewStack[int]()
-
-		item1 := 1
-		item2 := 2
-		item3 := 3
-
-		s.Push(item1)
-		s.Push(item2)
-		s.Push(item3)
-
+		s, _ := newIntStack(3)
 		s.Delete(2)
 
-		if s.Size() != 2 {
-			t.Errorf("s.Size() = %v, want %v", s.Size(), 2)
+		if size := s.Size(); size != 2 {
+			t.Errorf("s.Size() = %v, want %v", size, 2)
 		}
 
-		if s.Pop() != item3 {
-			t.Errorf("s.Peek() = %v, want %v", s.Peek(), item3)
+		if pop := s.Pop(); pop != 3 {
+			t.Errorf("s.Peek() = %v, want %v", pop, 3)
 		}
 
-		if s.Pop() != item2 {
-			t.Errorf("s.Peek() = %v, want %v", s.Peek(), item2)
+		if pop := s.Pop(); pop != 2 {
+			t.Errorf("s.Peek() = %v, want %v", pop, 2)
 		}
 	})
 }
@@ -200,25 +159,19 @@ func TestStack_Find(t *testing.T) {
 	t.Run("it should return false if the item hasnt been pushed on to the stack", func(t *testing.T) {
 		s := NewStack[int]()
 
-		vals := []int{1, 2, 3, 4, 5}
-		for _, v := range vals {
-			if s.Find(v) {
-				t.Errorf("s.Find(%v) = %v, want %v", v, s.Find(v), false)
+		for i := 0; i < 10; i++ {
+			if found := s.Find(i); found {
+				t.Errorf("s.Find(%v) = %v, want %v", i, found, false)
 			}
 		}
 	})
 
 	t.Run("it should return true if the item has been pushed on to the stack", func(t *testing.T) {
-		s := NewStack[int]()
-
-		vals := []int{1, 2, 3, 4, 5}
-		for _, v := range vals {
-			s.Push(v)
-		}
+		s, vals := newIntStack(5)
 
 		for _, v := range vals {
-			if !s.Find(v) {
-				t.Errorf("s.Find(%v) = %v, want %v", v, s.Find(v), true)
+			if found := s.Find(v); !found {
+				t.Errorf("s.Find(%v) = %v, want %v", v, found, true)
 			}
 		}
 	})
@@ -283,22 +236,31 @@ func TestStack_Max(t *testing.T) {
 
 func TestStack_Reverse(t *testing.T) {
 	t.Run("it should reverse the stack and return a new copy without modifying the underlying stack", func(t *testing.T) {
-		s := NewStack[int]()
-		for i := 0; i <= 10; i++ {
-			s.Push(i)
-		}
+		s, _ := newIntStack(10)
 
 		rev := s.Reverse()
-		for i := 0; i <= 10; i++ {
+		for i := 1; i <= 10; i++ {
 			if pop := rev.Pop(); pop != i {
 				t.Errorf("rev.Pop() = %v, want %v", pop, i)
 			}
 		}
 
-		for i := 10; i >= 0; i-- {
+		for i := 10; i >= 1; i-- {
 			if pop := s.Pop(); pop != i {
 				t.Errorf("s.Pop() = %v, want %v", pop, i)
 			}
 		}
 	})
+}
+
+func newIntStack(n int) (*Stack[int], []int) {
+	stack := NewStack[int]()
+	vals := []int{}
+
+	for i := 1; i <= n; i++ {
+		stack.Push(i)
+		vals = append(vals, i)
+	}
+
+	return stack, vals
 }
