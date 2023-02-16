2438f51050bf0080e29caa526a6821661fffbbf7
sanevillain <denis.ismail@code.berlin>
2023-02-16 12:21:59 +0100

    add node comments

--- bags/linkedlist/node.go
+++ bags/linkedlist/node.go
@@ -2,10 +2,10 @@ package linkedlist
 
 import "golang.org/x/exp/constraints"
 
-// Node is a node in a linked list
+// Node is a node in a linked list that keeps a reference to the node after it
 type Node[T constraints.Ordered] struct {
-	item T
-	next *Node[T]
+	item T        // item of the node
+	next *Node[T] // next node in the list
 }
 
 // NewNode creates a new node.
@@ -33,21 +33,16 @@ func (n *Node[T]) Next() *Node[T] {
 	return n.next
 }
 
-// DoubleNode is a node in a linked list
+// DoubleNode is a node in a linked list that keeps a references to both the node before and after it
 type DoubleNode[T constraints.Ordered] struct {
-	item T
-	next *DoubleNode[T]
-	prev *DoubleNode[T]
+	item T              // item of the node
+	next *DoubleNode[T] // next node in the list
+	prev *DoubleNode[T] // previous node in the list
 }
 
 // Item returns the item of the node.
 func (n *DoubleNode[T]) Item() T {
-	if n != nil {
-		return n.item
-	}
-
-	var empty T
-	return empty
+	return n.item
 }
 
 // Next returns the next node.
@@ -72,17 +67,25 @@ func (n *DoubleNode[T]) SetPrev(prev *DoubleNode[T]) {
 
 // Remove removes the prev and next references and returns the item
 func (n *DoubleNode[T]) Remove() T {
+	// removing a double node from a list requires linking the previous and next nodes to one another (skipping the node that we want to remove)
+
 	if n.prev != nil {
+		// always make sure to check for nil references before setting
+		// the previous node's next field to point to the one after next
 		n.prev.next = n.next
 	}
 
 	if n.next != nil {
+		// always make sure to check for nil references before setting
+		// the next node's previous field to point to the one before prev
 		n.next.prev = n.prev
 	}
 
+	// remove the references to the previous and next nodes (optional)
 	n.next = nil
 	n.prev = nil
 
+	// return the item it holds
 	return n.item
 }
 
